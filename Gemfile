b59b9b9bfbe029727b3fcec68c4308bcc3d5f0d7
Katsunori Kawaguchi <hamakata@gmail.com>
2014-09-02 23:25:44 +0900

    Initial

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source 'https://rubygems.org'
+
+ruby '2.1.1'
+
+gem 'sinatra'
+gem 'sinatra-contrib'
+gem 'haml'
+
+group :development, :test do
+  gem 'pry'
+  gem 'shotgun'
+end
+
+group :production do
+  gem 'unicorn'
+end
---
+++ app.rb
@@ -0,0 +1,35 @@
+require "sinatra/base"
+require "sinatra/cookies"
+require "json"
+
+class App < Sinatra::Base
+  helpers Sinatra::Cookies
+
+  get "/" do
+    @jj_cookie_json = JSON.pretty_generate(cookies.to_h)
+    puts cookies.class
+    haml :index
+  end
+
+  get "/new" do
+    @cookie_str = "{\"hoge\":1}"
+    haml :edit
+  end
+
+  post "/create" do
+    cookies.clear
+    begin
+      JSON.parse(params["cookie_json"]).each do |k, v|
+        cookies[k] = v
+      end
+      redirect to("/")
+    rescue
+      redirect to("/new")
+    end
+  end
+
+  get "/destroy" do
+    cookies.clear
+    redirect to("/")
+  end
+end
